39c2ceef2b02bf5bce92c4ac0f15240629e9410a
Admin <Admin@Admin-PC>
2020-09-07 14:40:45 +0530

    commit bynikita4-sep

---
+++ GitHub/src/gitDemo/Demo.java
@@ -0,0 +1,10 @@
+package gitDemo;
+
+public class Demo {
+	public void sum() {
+		System.out.println("Summ method");
+	}
+public static void main(String[] args) {
+	System.out.println("Hello_Main Mehod");
+}
+}
